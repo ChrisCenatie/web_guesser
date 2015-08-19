5de3dcb4e06cb4e084c5eea6783b91ac2319931d
Chris Cenatiempo <etothe2pieye@gmail.com>
2015-08-18 20:11:39 -0600

    Added Color Extension

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,21 +6,29 @@ SECRET_NUMBER = rand(100)
 get '/' do
   guess = params["guess"]
   message = check_guess(guess)
-  erb :index, :locals => { :number => SECRET_NUMBER, :message => message}
+  erb :index, :locals => { :number => SECRET_NUMBER,
+                           :message => message,
+                           :color => @color}
 end
 
 def check_guess(guess)
   if guess.nil?
+    @color = '#FFFFFF'
     ""
   elsif guess.to_i > SECRET_NUMBER + 5
+    @color = '#FF0000'
     "Way too high!"
   elsif guess.to_i > SECRET_NUMBER && guess.to_i <= SECRET_NUMBER + 5
+    @color = "#FF9090"
     "Too high!"
   elsif guess.to_i < SECRET_NUMBER - 5
+    @color = '#FF0000'
     "Way too low!"
   elsif guess.to_i < SECRET_NUMBER && guess.to_i >= SECRET_NUMBER - 5
+    @color = "#FF9090"
     "Too low!"
   else
+    @color = '#00FF00'
     "The SECRET NUMBER is"
   end
 end
