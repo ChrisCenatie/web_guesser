f871b28888d6879699cd39a98d6839b12d06c851
Chris Cenatiempo <etothe2pieye@gmail.com>
2015-08-19 12:48:29 -0600

    added guess limiting function

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,34 +1,55 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(100)
+@@secret_number = rand(100)
+@@count = 5
 
 get '/' do
   guess = params["guess"]
   message = check_guess(guess)
-  erb :index, :locals => { :number => SECRET_NUMBER,
+  count_message = check_count(guess)
+  erb :index, :locals => { :number => @@secret_number,
                            :message => message,
-                           :color => @color}
+                           :color => @color,
+                           :count_message => count_message
+                         }
 end
 
 def check_guess(guess)
   if guess.nil?
     @color = '#FFFFFF'
     ""
-  elsif guess.to_i > SECRET_NUMBER + 5
+  elsif guess.to_i > @@secret_number + 5
     @color = '#FF0000'
     "Way too high!"
-  elsif guess.to_i > SECRET_NUMBER && guess.to_i <= SECRET_NUMBER + 5
+  elsif guess.to_i > @@secret_number && guess.to_i <= @@secret_number + 5
     @color = "#FF9090"
     "Too high!"
-  elsif guess.to_i < SECRET_NUMBER - 5
+  elsif guess.to_i < @@secret_number - 5
     @color = '#FF0000'
     "Way too low!"
-  elsif guess.to_i < SECRET_NUMBER && guess.to_i >= SECRET_NUMBER - 5
+  elsif guess.to_i < @@secret_number && guess.to_i >= @@secret_number - 5
     @color = "#FF9090"
     "Too low!"
   else
     @color = '#00FF00'
     "The SECRET NUMBER is"
+  end
+end
+
+def check_count(guess)
+  if guess.nil?
+    ""
+  elsif @@count > 1 && guess.to_i != @@secret_number
+    @@count = @@count - 1
+    "You now have #{@@count} tries left"
+  elsif @@count > 0 && guess.to_i == @@secret_number
+    @@count = 5
+    @@secret_number = rand(100)
+    "You've guessed correctly. A new number has been generated."
+  elsif @@count == 1 && guess.to_i != @@secret_number
+    @@count = 5
+    @@secret_number = rand(100)
+    "Too many tries! A new number has been generated"
   end
 end
